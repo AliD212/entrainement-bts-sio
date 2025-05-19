2a50801710719c28105e6e287ea2bae89a9f4a01
AliD212 <149754589+AliD212@users.noreply.github.com>
2025-05-19 17:23:56 +0200

    Update users.php

--- users.php
+++ users.php
@@ -21,9 +21,25 @@ $users = $_SESSION['users'];
 
     <p><a href="index.php">Retour à l'ajout d'un utilisateur</a></p>
 
-    <form action="consignes.php" method="POST" name="supprimer">
+    <form action="bouton.php" method="POST" name="delete">
         <input type="text" name="user">
         <button type="submit">Supprimer</button>
+    </form>
+
+
+    <form action="update_user.php" method="POST" name="update">
+        <label>nom</label>
+        <input type="text" name="old_name">
+        <br>
+        <label>nouveau nom</label>
+        <input type="text" name="new_name">
+        <br>
+        <label>email</label>
+        <input type="text" name="old_email">
+        <br>
+        <label>nouvelle email</label>
+        <input type="text" name="new_email">
+        <button type="submit">modifier</button>
     </form>
 
 </body>
