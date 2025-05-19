24958999198edf1e7edd229c05a8244c51c14887
AliD212 <149754589+AliD212@users.noreply.github.com>
2025-05-19 17:23:25 +0200

    Update update_user.php

--- update_user.php
+++ update_user.php
@@ -1,31 +1,46 @@
 <?php
 session_start();
-$tableau_user = $_SESSION['users'];
-
-function update_user($new_name)
+function update_name_user($old_name, $new_name)
 {
-    $tableau_user = &$_SESSION['users'];
-    $update_name = $_POST['update_name'];
-    $email = $_POST['name'];
-
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $new_name = trim($new_name);
-        $old_name = trim($_POST['name']);
+        $old_name = trim($_POST['old_name']);
+        $new_name = trim($_POST['new_name']);
+        $tableau_user = &$_SESSION['users'];
         foreach ($tableau_user as $key => &$user) {
             if ($user['name'] == $old_name) {
                 echo "L'utilisateur " . $user['name'] . " à était modifier";
-                $tableau_users = $tableau_user[$key];
-                $tableau_users['name'] = $new_name;
-
+                $user['name'] = $new_name;
                 return 1;
             }
         }
     }
 }
-var_dump($_POST['update_name']);
-update_user($_POST['update_name']);
 
-//var_dump(update_user($_POST['name']));
+var_dump(!empty($_POST['old_name']));
+
+if (!empty($_POST['old_name']) == TRUE && !empty($_POST['new_name']) == TRUE) {
+    update_name_user($_POST['old_name'], $_POST['new_name']);
+}
+
+
+function update_email_user($old_email, $new_email)
+{
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $old_email = trim($_POST['old_email']);
+        $new_email = trim($_POST['new_email']);
+        $tableau_user = &$_SESSION['users'];
+        foreach ($tableau_user as $key => &$user) {
+            if ($user['email'] == $old_email) {
+                echo "L'email de l'utilisateur " . $user['name'] . " à était modifier";
+                $user['email'] = $new_email;
+                return 1;
+            }
+        }
+    }
+}
+
+update_email_user($_POST['old_email'], $_POST['new_email']);
+
 
 
 ?>
