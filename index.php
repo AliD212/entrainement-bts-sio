56bcae3373c6ee08224c48b043878085e9176fbb
AliD212 <149754589+AliD212@users.noreply.github.com>
2025-05-23 17:39:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,10 @@
 
 
 // Initialisation du tableau d'utilisateurs
+
 session_start();
+
+/* 
 
 if (!isset($_SESSION['users'])) {
     $_SESSION['users'] = [];
@@ -35,6 +38,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'email' => $email
     ];
 }
+
+*/
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    try {
+
+        $nom = $_POST['name'];
+        $email = $_POST['email'];
+
+        $bd = new PDO('mysql:host=localhost;dbname=bd', 'root', '');
+        $sql_insert = "INSERT INTO user (id, nom, email) VALUES (:id, :nom, :email)";
+        $sql_select_max_id = "select max(id) from user";
+
+
+        // récupération du résultat qui est le chiffre max de la colonne id de la table user
+
+        $resultat = $bd->query($sql_select_max_id);
+        $resultat = $resultat->fetch();
+
+        // addition du chiffre max + 1
+
+        $id =  $resultat['max(id)'] + 1;
+
+
+        $stmt = $bd->prepare($sql_insert);
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+    } catch (PDOException $pe) {
+
+        die("Could not connect to the database $dbname :" . $pe->getMessage());
+    }
+}
+
 
 ?>
 
@@ -60,18 +98,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <button type="submit">Ajouter</button>
     </form>
 
+
+    <p><a href="users.php">Voir la liste des utilisateurs</a></p>
+
+
     <h1>Trouver un utilisateur</h1>
 
     <form method="POST" action="search_user.php">
         <label for="name">Email :</label>
-        <input type="text" name="email" required>
+        <input type="text" name="search_email" required>
         <br>
         <button type="submit">Chercher</button>
     </form>
-
-
-
-    <p><a href="users.php">Voir la liste des utilisateurs</a></p>
 
 </body>
 
