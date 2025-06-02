c9dbcec898b60b2d966e3166d7836e1ab72243c7
AliD212 <149754589+AliD212@users.noreply.github.com>
2025-06-02 17:16:33 +0200

    Update update_user.php

--- update_user.php
+++ update_user.php
@@ -1,22 +1,18 @@
 <?php
-session_start();
 function update_name_user($old_name, $new_name)
 {
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $old_name = trim($_POST['old_name']);
-        $new_name = trim($_POST['new_name']);
-        $tableau_user = &$_SESSION['users'];
-        foreach ($tableau_user as $key => &$user) {
-            if ($user['name'] == $old_name) {
-                echo "L'utilisateur " . $user['name'] . " à était modifier";
-                $user['name'] = $new_name;
-                return 1;
-            }
-        }
+
+    $bd  = new PDO('mysql:host=localhost;dbname=bd', 'root', '');
+    $old_name = trim($_POST['old_name']);
+    $new_name = trim($_POST['new_name']);
+    if (!empty($old_name && $new_name)) {
+        $sql_update = "update user set nom = '$new_name' where nom = '$old_name'";
+        $user_update = $bd->query($sql_update);
+        $user_update->execute();
+        echo "Le nom de l'utilisateur à était modifié par", $new_name;
     }
 }
 
-var_dump(!empty($_POST['old_name']));
 
 if (!empty($_POST['old_name']) == TRUE && !empty($_POST['new_name']) == TRUE) {
     update_name_user($_POST['old_name'], $_POST['new_name']);
@@ -25,17 +21,23 @@ if (!empty($_POST['old_name']) == TRUE && !empty($_POST['new_name']) == TRUE) {
 
 function update_email_user($old_email, $new_email)
 {
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $old_email = trim($_POST['old_email']);
-        $new_email = trim($_POST['new_email']);
-        $tableau_user = &$_SESSION['users'];
-        foreach ($tableau_user as $key => &$user) {
-            if ($user['email'] == $old_email) {
-                echo "L'email de l'utilisateur " . $user['name'] . " à était modifier";
-                $user['email'] = $new_email;
-                return 1;
-            }
-        }
+
+
+    $bd  = new PDO('mysql:host=localhost;dbname=bd', 'root', '');
+    $old_email = trim($_POST['old_email']);
+    $new_email = trim($_POST['new_email']);
+    $sql_select_all = "SELECT * FROM user WHERE email = :email";
+    $stmt = $bd->prepare($sql_select_all);
+    $stmt->execute([':email' => $new_email]);
+    $user = $stmt->fetch();
+
+    if ($user) {
+        echo "L'email $new_email est déjà utilisé";
+    } else {
+        $sql_update = "UPDATE user SET email = :new_email WHERE email = :old_email";
+        $stmt_update = $bd->prepare($sql_update);
+        $stmt_update->execute([':new_email' => $new_email, ':old_email' => $old_email]);
+        echo "Le mail de l'utilisateur a été modifié en $new_email";
     }
 }
 
